419e435d23e75e36c668ebfb4202356e340389f0
mharjoliee <mharjsandel@gmail.com>
2018-07-07 18:41:09 +0800

    Fourth commit

--- app/src/main/java/com/example/kayletiu/samcompanion/companion_activity.java
+++ app/src/main/java/com/example/kayletiu/samcompanion/companion_activity.java
@@ -3,10 +3,13 @@ package com.example.kayletiu.samcompanion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class companion_activity extends AppCompatActivity {
 
     //Firebase db variables
-    private FirebaseDatabase database = FirebaseDatabase.getInstance{};
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference myRef = database.getReference("message");
 
     @Override
