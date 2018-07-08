e7447434c8db0a3ccf767b4c839c4e4e536cf934
jason_deichmann@dlsu.edu.ph <jason_deichmann@dlsu.edu.ph>
2018-07-08 18:17:47 +0800

    SHealth libs added

--- app/src/main/java/com/example/kayletiu/samcompanion/Exercise.java
+++ app/src/main/java/com/example/kayletiu/samcompanion/Exercise.java
@@ -9,10 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.samsung.android.sdk.shealth.Shealth;
-import com.samsung.android.sdk.shealth.tracker.TrackerInfo;
-import com.samsung.android.sdk.shealth.tracker.TrackerManager;
-
 public class Exercise extends AppCompatActivity {
 
     @Override
@@ -24,21 +20,8 @@ public class Exercise extends AppCompatActivity {
             SHEALTHbtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    getRunningTrackerInfo();
                 }
             });
         }
 
-    public TrackerInfo getRunningTrackerInfo() {
-        // Create a TrackerManager instance
-        TrackerManager trackerManager = new TrackerManager(getApplicationContext());
-
-        // Get the "Running" tracker info's instance
-        TrackerInfo trackerInfo = trackerManager.getTrackerInfo(TrackerManager.TrackerId.RUNNING);
-
-        if(trackerInfo.isAvailable())
-            return trackerInfo;
-        else
-            return null;
-    }
     }
